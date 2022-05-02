f4267ddbe25f6055a04b43d29a5c56975a0fd237
Apiligu Asakiso <apullahalbert@gmail.com>
2022-05-02 18:03:56 +0000

    second commit after setup

---
+++ src/api/fakeplayer/controllers/fakeplayer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  fakeplayer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fakeplayer.fakeplayer');
---
+++ src/api/fakeplayer/routes/fakeplayer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fakeplayer router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::fakeplayer.fakeplayer');
