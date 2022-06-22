9302f8a54242c349eecf4f52319d7b900e1203f2
Apiligu Asakiso <apullahalbert@gmail.com>
2022-06-22 14:31:42 +0000

    test heroku commit

--- config/server.js
+++ config/server.js
@@ -10,3 +10,9 @@ module.exports = ({ env }) => ({
 //   host: env("APP_HOST", "0.0.0.0"),
 //   port: env.int("NODE_PORT", 1337),
 // });
+
+
+// module.exports = ({ env }) => ({
+//   host: env('APP_HOST', '0.0.0.0'),
+//   port: env.int('NODE_PORT', 1337),
+// });
